f26e072ce397ff99c6b0fa5057cadfc22210b81d
dalizi <dalizi@lirongdeMacBook-Pro.local>
2018-12-26 11:21:14 +0800

    Initial Commit

---
+++ LROCAlertController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LROCAlertController
+//
+//  Created by dalizi on 2018/12/26.
+//  Copyright © 2018年 dalizi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LROCAlertController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LROCAlertController
+//
+//  Created by dalizi on 2018/12/26.
+//  Copyright © 2018年 dalizi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
